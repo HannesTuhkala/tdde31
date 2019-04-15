afb5405b000c072413d2603d21bac295e5716f77
Robin Sliwa <rorrosliwa@gmail.com>
2019-04-15 13:50:35 +0200

    Include station number

--- lab1/exercise1.py
+++ lab1/exercise1.py
@@ -1,27 +1,27 @@
 from pyspark import SparkContext
 
 def max_temperature(a, b):
-	if a >= b:
-		return a
-	else:
-		return b
+    if a >= b:
+        return a
+    else:
+        return b
 
 def min_temperature(a, b):
-	if a <= b:
-		return a
-	else:
-		return b
+    if a <= b:
+        return a
+    else:
+        return b
 
 sc = SparkContext(appName = "exercise 1")
-temperature_file = sc.textFile("/user/x_hantu/data/temperature-readings.csv")
+temperature_file = sc.textFile("/user/x_robsl/data/temperature-readings.csv")
 lines = temperature_file.map(lambda line: line.split(";"))
-year_temperature = lines.map(lambda x: (x[1][0:4], float(x[3], x[0])))
+year_temperature = lines.map(lambda x: (x[1][0:4], (float(x[3]), x[0])))
 year_temperature = year_temperature.filter(lambda x: int(x[0]) >= 1950 and int(x[0]) <= 2014)
 
 max_temperatures = year_temperature.reduceByKey(max_temperature)
-max_temperaturesSorted = max_temperatures.sortBy(ascending = False, keyfunc=lambda k: k[1])
+max_temperaturesSorted = max_temperatures.sortBy(ascending = False, keyfunc=lambda k: k[1][0])
 min_temperatures = year_temperature.reduceByKey(min_temperature)
-min_temperaturesSorted = min_temperatures.sortBy(ascending = False, keyfunc=lambda k: k[1])
+min_temperaturesSorted = min_temperatures.sortBy(ascending = False, keyfunc=lambda k: k[1][0])
 
 max_temperaturesSorted.saveAsTextFile("max_temperature")
 min_temperaturesSorted.saveAsTextFile("min_temperature")
