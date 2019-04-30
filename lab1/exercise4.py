231b1fa3c46bb5aa6f01b98a1f5f030144edc761
Hannes Tuhkala <hantu447@student.liu.se>
2019-04-30 15:00:16 +0200

    Added exercise 4 for lab1

---
+++ lab1/exercise4.py
@@ -0,0 +1,49 @@
+from pyspark import SparkContext
+temp_file = "/user/x_hantu/data/temperature-readings.csv"
+prec_file = "/user/x_hantu/data/precipitation-readings.csv"
+
+
+def max_temperature(temp):
+    return temp >= 25 and temp <= 30
+
+
+def max_precipitation(prec):
+    return prec >= 100 and prec <= 200
+
+
+# Find the value of tuple b with the "key" key.
+def getItem(key, b):
+	for item in b:
+		if item[0] == key:
+			return item[1]
+
+
+# Combines the two tuples into one as (number, max_temp, max_prec)
+def combine(a, b):
+	result = []
+	for item in a:
+		if (item[0] in [i[0] for i in b]):
+			result += [item[0], item[1], getItem(item[0], b)]
+
+
+sc = SparkContext(appName = "exercise 4")
+temperature_file = sc.textFile(temp_file)
+temp_lines = temperature_file.map(lambda line: line.split(";"))
+# Creates a tuple as (stationnumber, max_temp)
+year_temperature = temp_lines.map(lambda x: (x[0], float(x[3])))
+# Removes all tuples that do not satisfy max_temperature
+max_temp = year_temperature.reduceByKey(max_temperature)
+
+
+precipitation_file = sc.textFile(prec_file)
+prec_lines = precipitation_file.map(lambda line: line.split(";"))
+# Creates a tuple as (stationnumber, max_prec)
+year_prec = prec_lines.map(lambda x: (x[0], float(x[3])))
+# Removes all tuples that do not satisfy max_precipitation
+max_prec = year_prec.reduceByKey(max_precipitation)
+
+# Combine the two tuples into (number, max_temp, max_prec)
+# We have to use an existing function to be able to map using their data objects
+# look at https://spark.apache.org/docs/latest/rdd-programming-guide.html#transformations
+result = combine(max_temp, max_prec)
+result.saveAsTextFile("max_temp_prec")
