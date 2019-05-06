36ed898dcf51034fe55e0174cbb3d1a7dca2b25c
Hannes Tuhkala <hantu447@student.liu.se>
2019-05-06 12:46:52 +0200

    Updated exercise 5

--- lab1/exercise5.py
+++ lab1/exercise5.py
@@ -1,6 +1,6 @@
 from pyspark import SparkContext
-STAT_FILE = "/users/x_hantu/data/stations-Ostergotland.csv"
-PREC_FILE = "/users/x_hantu/data/precipitation-readings.csv"
+STAT_FILE = "/user/x_hantu/data/stations-Ostergotland.csv"
+PREC_FILE = "/user/x_hantu/data/precipitation-readings.csv"
 
 sc = SparkContext(appName = "exercise 5")
 
@@ -28,32 +28,45 @@ def comb_op(accumulator1, accumulator2):
 stations_file = sc.textFile(STAT_FILE)
 stat_lines = stations_file.map(lambda line: line.split(";"))
 
-precipitations_file = sc.textFile(STAT_FILE)
-prec_lines = stations_file.map(lambda line: line.split(";"))
+stat_data = stat_lines.map(lambda x: x[0])
+
+precipitations_file = sc.textFile(PREC_FILE)
+prec_lines = precipitations_file.map(lambda line: line.split(";"))
 
 prec_data = prec_lines.map(lambda x: (x[0], (x[1], x[3])))
 # Filter out the data outside the wanted dates
 prec_data = prec_data.filter(lambda x: int(x[1][0][0:4]) >= 1993 and int(x[1][0][0:4]) <= 2016)
 
-# unsure
-prec_data.filter(lambda x: x[0] in stat_lines[0])
+
+########################################
+THIS IS WHERE IT FAILS. IN THE FILTER.
+#########################################
+
+# Filter only those station numbers that are in STAT_FILE.
+prec_data = prec_data.filter(lambda x: x[0] in stat_data)
+# We remove the stationnumber, since it is not required anymore
+# New structure: (date, value)
 prec_data.map(lambda x: (x[1][0], x[1][1]))
+
+prec_data.saveAsTextFile("test11")
 
 # Only save tuples that has the max and min temperatures for that day
 max_day_precipitations = prec_data.reduceByKey(max_precipitation)
 min_day_precipitations = prec_data.reduceByKey(min_precipitation)
 
-# Combine max and min into the following tuple (key, (max_temp, min_temp))
+# Combine max and min into the following tuple (date, (max_prec, min_prec))
 precipitations = max_day_precipitations.join(min_day_precipitations)
 
-# Removes the day from date, all grouped up by (year-month, stnnumber) now.
+# Removes the day from date, structure is now (year-month, (max_prec, min_prec))
 precipitations = precipitations.map(lambda x: (x[0][0:7], x[1]))
 
-# Calculates how many days there are in each month with the following output ((year-month, stationnumber), daysInMonth)
+# Calculates how many days there are in each month with the following output (year-month, daysInMonth)
 days = precipitations.countByKey().items()
 ndays = sc.parallelize(days)
 
-# Calculates the sum of all max and min temp values.
+precipitations.saveAsTextFile("test10")
+
+# Calculates the sum of all max and min prec values per month.
 average_prec = precipitations.aggregateByKey(0, seq_op, comb_op)
 average_prec = average_prec.join(ndays)
 
