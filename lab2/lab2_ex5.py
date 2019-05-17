b600e44cec95f79adbe1f60dbdb4d6d868c25de6
Hannes Tuhkala <hantu447@student.liu.se>
2019-05-17 13:34:16 +0200

    Added lab2 ex5

---
+++ lab2/lab2_ex5.py
@@ -0,0 +1,23 @@
+from pyspark import SparkContext
+from pyspark.sql import SQLContext, Row
+from pyspark.sql import functions as F
+
+sc = SparkContext(appName = "exercise 5")
+sqlContext = SQLContext(sc)
+station_file = sc.textFile("/user/x_hantu/data/stations-Ostergotland.csv")
+stat_lines = station_file.map(lambda line: line.split(";"))
+statReadings = temp_lines.map(lambda p: Row(station=int(p[0])))
+schemaStatReadings = sqlContext.createDataFrame(statReadings)
+schemaStatReadings.registerTempTable("statReadings")
+
+precipitation_file = sc.textFile("/user/x_hantu/data/precipitation-readings.csv")
+prec_lines = precipitation_file.map(lambda line: line.split(";"))
+precReadings = prec_lines.map(lambda p: Row(station=int(p[0]), year=int(p[1][0:4]), month=int(p[1][5:7]), prec=float(p[3])))
+schemaPrecReadings = sqlContext.createDataFrame(precReadings)
+schemaPrecReadings.registerTempTable("precReadings")
+
+valid_precipitations = schemaPrecReadings.where("year >= 1993 and year <= 2016")
+precipitations = valid_precipitations.join(schemaStatReadings, 'station', 'inner')
+
+precipitation = precipitation.groupBy('year', 'month', 'station').agg(F.sum('prec').alias('prec')
+precipitation = precipitation.groupBy('year', 'month').agg(F.avg('prec').alias('avgPrec')).orderBy(['year', 'month'], ascending=False).show()
